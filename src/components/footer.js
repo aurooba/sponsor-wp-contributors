6efd2596f6b3e0042bf73ee5896a63e73fcaa1a5
Aurooba Ahmed <hey@aurooba.com>
2023-03-12 19:57:05 -0600

    add footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<p>
+				<a href="https://github.com/aurooba/sponsor-wp-contributors">
+					Add yourself here
+				</a>
+			</p>
+		</footer>
+	);
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
 import * as React from "react";
 import ContributorList from "../components/contributor-list";
 import Search from "../components/search";
+import Footer from "../components/footer";
 import "../styles/global.scss";
 
 const IndexPage = () => {
 	return (
 		<main>
 			<h1>Sponsor WordPress Contributors</h1>
-			<Search />
-			<section className="contributor-list">
-				<ContributorList />
-			</section>
+			<div className="site-content">
+				<Search />
+				<section className="contributor-list">
+					<ContributorList />
+				</section>
+			</div>
+
+			<Footer />
 		</main>
 	);
 };
